e5496822b4b22fe528c09a26d796e35def5ff901
allys <alicia.rsouza@gmail.com>
2024-04-17 17:57:45 -0300

    refactor: funcao de gerar numero

--- cypress/support/numeroAleatorio.js
+++ cypress/support/numeroAleatorio.js
@@ -1,3 +1,12 @@
 export const gerarNumeroAleatorio = () => {
-    return Math.floor(Math.random() * 10000) + 1;
-}
+  const numerosGerados = {};
+
+  while (true) {
+    const numeroAleatorio = Math.floor(Math.random() * 10000) + 1;
+
+    if (!numerosGerados[numeroAleatorio]) {
+      numerosGerados[numeroAleatorio] = true;
+      return numeroAleatorio;
+    }
+  }
+};
